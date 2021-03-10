73176e5b3edd807a4841d37fcb605a8d4c93aa29
Bencin K. Shaji <mca512@rcss.com>
2021-03-10 15:18:34 +0530

    message

---
+++ container.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="container.html">rajagiri</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="student.html">student</a>
+              </li>
+              
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <div class="container">
+        <div class="row">
+          <div class="col col-12 col-sm-6 col-md-6 col-lg-6"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore labore ad quos animi nulla laborum cumque accusamus nam voluptate obcaecati sunt consequuntur pariatur, esse autem reiciendis molestiae quam, vel necessitatibus!</div>
+          <div class="col col-12 col-sm-6 col-md-6 col-lg-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quae blanditiis earum deserunt. Inventore adipisci maxime nihil nostrum qui? Porro ipsam aliquam placeat id consectetur. Dolorum a iure ipsum eveniet.</div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
